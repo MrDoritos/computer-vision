e0b48a2f10735e41138610588f58ae63bff7c1ec
MrDoritos <ianmoore322@gmail.com>
2024-05-03 15:15:54 -0700

    OpenCV detects 8x8 ChArUco

--- display-charuco-board.py
+++ display-charuco-board.py
@@ -4,8 +4,11 @@ import numpy as np
 import cv2
 
 # Load ChArUco board parameters
-dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
-board = cv2.aruco.CharucoBoard_create(7, 5, 0.04, 0.02, dictionary)
+#cv2.aruco.DICT_7X7_50
+#dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+#board = cv2.aruco.CharucoBoard_create(7, 5, 0.04, 0.02, dictionary)
+board = cv2.aruco.CharucoBoard([8,8], 0.019, 0.014, cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_100))
+board.setLegacyPattern(True)
 
 # Create video capture object
 cap = cv2.VideoCapture(0)
@@ -19,11 +22,15 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect markers and corners
-    corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, dictionary)
+    corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, board.getDictionary())
     if len(corners) > 0:
         ret, charuco_corners, charuco_ids = cv2.aruco.interpolateCornersCharuco(corners, ids, gray, board)
-        if ret > 20:
+        #print("corners: ", corners, " ids: ", ids, " rejectedImgPoints: ", rejectedImgPoints)
+        print("ret: ", ret, " charuco_corners: ", charuco_corners, " charuco_ids: ", charuco_ids)
+        if ret > 0:
             frame = cv2.aruco.drawDetectedCornersCharuco(frame, charuco_corners, charuco_ids)
+        else:
+            frame = gray
 
     # Display the frame
     cv2.imshow('ChArUco Detection', frame)
